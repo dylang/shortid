d3ec289d127d9569093cae3e7d17f573c4e41df0
Alessandro Segala <EgoAleSum@users.noreply.github.com>
2015-01-17 19:54:15 -0500

    Added browserify grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,39 @@ module.exports = function (grunt) {
 
     require('time-grunt')(grunt);
 
+    var derequire = require('derequire');
+    var envify = require('envify/custom');
+
+    var envifyTransform = envify({NODE_UNIQUE_ID: 0})
+
     grunt.initConfig({
+        browserify: {
+            dist: {
+                src: ['lib/shortid.js'],
+                dest: 'shortid.js'
+            },
+            options: {
+                browserifyOptions: {
+                    standalone: 'generate',
+                    transform: [envifyTransform]
+                },
+                external: ['crypto'], // Mark 'crypto' as external so browserify doesn't load its (huge) shim
+                postBundleCB: function(err, src, next) {
+                    // Run derequire
+                    var modifiedSrc = false;
+
+                    if(!err) {
+                        modifiedSrc = derequire(src);
+                        if(!modifiedSrc) {
+                            err = true;
+                        }
+                    }
+
+                    next(err, modifiedSrc);
+                }
+            }
+        },
+
         mochaTest: {
             notify: {
                 src: 'test/**/*.test.js',
